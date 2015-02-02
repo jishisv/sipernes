ef66ebc2dd06c1b9b40a36fdd12005b5e9a0d89a
juan carlose <juan.serrano.sv@gmail.com>
2015-02-01 23:39:42 -0600

    correccion en menu

--- src/Minsal/Sipernes/ReportesBundle/Admin/EnfHistoricoCapacitacionAdmin.php
+++ src/Minsal/Sipernes/ReportesBundle/Admin/EnfHistoricoCapacitacionAdmin.php
@@ -97,7 +97,7 @@ class EnfHistoricoCapacitacionAdmin extends Admin
         switch ($name) {
             case 'list':
 
-                return 'MinsalSipernesReportesBundle:Reportes:reporte_diario_disc_vac.html.twig';
+                return 'MinsalSipernesReportesBundle:Reportes:reporte_pac_diag.html.twig';
 
               //  return 'MinsalSipernesReportesBundle:Reportes:reporte_sema_anot.html.twig';
 
