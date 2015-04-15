b0d5fc84c5a3cdc90ce80c3b447ff575a3981e47
juan carlose <juan.serrano.sv@gmail.com>
2015-04-14 23:08:21 -0600

    cambios en reportes consolidado de anotaciones , micronutrientes aplicados, y consolidado diario de aplicacion de micronutrientes

--- src/Minsal/Sipernes/ReportesBundle/Controller/ReporteMetodoController.php
+++ src/Minsal/Sipernes/ReportesBundle/Controller/ReporteMetodoController.php
@@ -198,8 +198,8 @@ class ReporteMetodoController extends Controller {
         $jasperReport->setReportFormat($report_format);
         $jasperReport->setReportPath("/reports_siaps_seguimiento/siaps/seguimiento/");
         $jasperReport->setReportParams(array(
-            'fecha_inicio' => $fecha_inicio,
-            'fecha_fin' => $fecha_fin,
+            'fpini' => $fecha_inicio,
+            'fpfin' => $fecha_fin,
             'deptos' => $deptos,
             'municipios' => $municipios,
             'establecimientos' => $establecimientos,
