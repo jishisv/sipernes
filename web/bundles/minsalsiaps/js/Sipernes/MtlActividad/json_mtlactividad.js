a07a2415ef4e27b565d84cd7394ff23558f552b8
juan carlose <juan.serrano.sv@gmail.com>
2015-05-26 21:18:07 -0600

    correccion de combos para mostrar codigo mas nombres

--- web/bundles/minsalsiaps/js/Sipernes/MtlActividad/json_mtlactividad.js
+++ web/bundles/minsalsiaps/js/Sipernes/MtlActividad/json_mtlactividad.js
@@ -16,8 +16,21 @@ $(document).ready(function() {
         allowClear: true,
         width: '100%'
     });
-
+    if($('select[id$="_idSubactividad"]').val() != 0){
+  /*CARGAR Actividad*/
+  alert ("entre al if");
  
+      $.getJSON(Routing.generate('get_actividad_padre') + '/' + $('select[id$="_idSubactividad"]').select2('val'),
+            function (data) {
+              
+             var pac;   
+               alert("entre");
+                $.each(data.datosactividad, function (indice, aux) {
+                
+                $('#actividad').append('<option value="' + aux.id + '">' + aux.idSubactividad.idActividad.nombreActividad + '</option>');
+                });
+            });
+        }else{
 
    //$('select[id$="_idIntervencion"]').attr('disabled', 'disabled');
    
@@ -28,11 +41,12 @@ $(document).ready(function() {
                     $('#actividad').append('<option value="' + aux.id + '">' + aux.nombreActividad + '</option>');
                 });
             });
-
+        }
 
     
     /*CARGAR SUBPROTOCOLOS*/
     $('#actividad').change(function() {
+        
         $('select[id$="_idSubactividad"]').children().remove();
         $('select[id$="_idSubactividad"]').append('<option></option>');
         $('select[id$="_idSubactividad"]').select2({
@@ -75,8 +89,30 @@ $(document).ready(function() {
                 });
 
     }
-    
+   
+    $('select[id$="_idSubactividad"]').change(function() {
+         $(this).remove();
+        $('select[id$="_idSubactividad"]').children().remove();
+        $('select[id$="_idSubactividad"]').append('<option></option>');
+        $('select[id$="_idSubactividad"]').select2({
+            placeholder: 'Seleccione Tipo de Actividad...',
+            allowClear: true,
+            width: '100%'
+        });
+       /*CARGAR PROTOCOLOS*/
+      $.getJSON(Routing.generate('get_Act'),
+            function (data) {
+                 $(this).remove();
+                $.each(data.actividad, function (indice, aux) {
+                    $('#actividad').append('<option value="' + aux.id + '">' + aux.nombreActividad + '</option>');
+                });
+            });
+        
+ 
+        
+   });
 
+    
   
 
 });
